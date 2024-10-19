c560c0b4fa6137fa4f327d428b241aebe1a79c1e
Anil <anil6996f@gmail.com>
2024-10-19 17:06:47 +0530

    implemented the ui for signin page and did the mutation from backend using react-query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import Layout from './layouts/Layout'
 import Register from "./pages/Register";
+import SignIn from "./pages/SignIn";
 
 const App = () => {
   return (
@@ -15,6 +16,11 @@ const App = () => {
         <Route path="/register" element={ 
           <Layout>
             <Register/>
+          </Layout>
+        } />
+        <Route path="/sign-in" element={ 
+          <Layout>
+            <SignIn/>
           </Layout>
         } />
         <Route path="*" element={<Navigate to='/'/>} />
