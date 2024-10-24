0f5c574fa91f632d5b51e0c6c374bded466705fb
Anil <anil6996f@gmail.com>
2024-10-24 13:26:39 +0530

    added the hotelTypes filter

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -5,12 +5,15 @@ import { useState } from "react";
 import SearchResultsCard from "../components/SearchResultsCard";
 import Pagination from "../components/Pagination";
 import StarRatingFilter from "../components/StarRatingFilter";
+import HotelTypesFilter from "../components/HotelTypesFilter";
 
 const Search = () => {
 
     const search = useSearchContext();
     const [page, setPage] = useState<number>(1);
     const [selectedStars, setSelectedStars] = useState<string[]>([]);
+    const [selectedHotelTypes, setSelectedHotelTypes] = useState<string[]>([]);
+
     const searchParams = {
         destination: search.destination,
         checkIn: search.checkIn.toISOString(),
@@ -18,7 +21,8 @@ const Search = () => {
         adultCount: search.adultCount.toString(),
         childCount: search.childCount.toString(),
         page: page.toString(),
-        stars:selectedStars,
+        stars: selectedStars,
+        types:selectedHotelTypes,
     };
 
 
@@ -32,12 +36,19 @@ const Search = () => {
         setSelectedStars((prevStars) => e.target.checked ? [...prevStars, starRating] : prevStars.filter((star) => star !== starRating)); //adding or removing stars
     }
 
+    const handleHotelTypeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const hotelType = e.target.value;
+        setSelectedHotelTypes((prevHotelTypes) => e.target.checked ? [...prevHotelTypes, hotelType] : prevHotelTypes.filter((type) => type !== hotelType)); //adding or removing stars
+    }
+
+    
     return (
         <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
             <div className="rounded-lg border border-slate-300 p-5 h-fit sticky top-10">
                 <div className="space-y-5">
                     <h3 className="text-lg font-semibold border-b border-slate-300 pb-5">Filter by:</h3>
-                    <StarRatingFilter selectedStars={selectedStars} onChange={handleStarsChange}/>
+                    <StarRatingFilter selectedStars={selectedStars} onChange={handleStarsChange} />
+                    <HotelTypesFilter selectedHotelTypes={selectedHotelTypes} onChange={handleHotelTypeChange}/>
                 </div>
             </div>
             <div className="flex flex-col gap-5">
